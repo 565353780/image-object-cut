a4d84dd75a4471e0201ef74c32e046c7f301b571
chLi <565353780@qq.com>
2022-09-27 15:09:36 +0800

    add basic image object cutter algo

---
+++ image_object_cut/Module/image_object_cutter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from image_object_cut.Config.color import H_RANGE_DICT
+
+
+class ImageObjectCutter(object):
+
+    def __init__(self, ):
+        return
