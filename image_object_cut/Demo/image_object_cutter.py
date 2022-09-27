d8e5a0d87570dccdd0652a3516ecba1ba2556eaf
chLi <565353780@qq.com>
2022-09-27 15:49:06 +0800

    update demo function call

--- image_object_cut/Demo/image_object_cutter.py
+++ image_object_cut/Demo/image_object_cutter.py
@@ -3,10 +3,14 @@
 
 from image_object_cut.Module.image_object_cutter import ImageObjectCutter
 
+
 def demo():
     image_file_path = "/home/chli/chLi/image_object_cut/1.jpg"
+    save_object_image_file_path = "/home/chli/chLi/image_object_cut/1_object.png"
 
     image_object_cutter = ImageObjectCutter()
 
-    object_image = image_object_cutter.cutImageFileObject(image_file_path, "green")
+    image_object_cutter.cutImageFileObject(image_file_path,
+                                           save_object_image_file_path,
+                                           "green")
     return True
