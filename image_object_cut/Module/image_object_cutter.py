ded910008a6e944afa3181318dfdeb88a07b8b71
chLi <565353780@qq.com>
2022-09-27 15:49:24 +0800

    finish cut image with alpha channel

--- image_object_cut/Module/image_object_cutter.py
+++ image_object_cut/Module/image_object_cutter.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
+
 import cv2
+import numpy as np
 
 from image_object_cut.Config.color import H_RANGE_DICT
+from image_object_cut.Method.path import createFileFolder
 
 
 class ImageObjectCutter(object):
@@ -12,8 +15,11 @@ class ImageObjectCutter(object):
     def __init__(self):
         return
 
-    def cutImageObject(self, image, color_mode):
-        assert color_mode in H_RANGE_DICT
+    def getColorModeList(self):
+        return list(H_RANGE_DICT.keys())
+
+    def getObjectImage(self, image, color_mode="green"):
+        assert color_mode in H_RANGE_DICT.keys()
 
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -22,19 +28,40 @@ class ImageObjectCutter(object):
 
         cv2.bitwise_not(mask, mask)
 
-        object_image = cv2.bitwise_and(image, image, mask=mask)
+        object_black_image = cv2.bitwise_and(image, image, mask=mask)
 
-        cv2.imshow("input", image)
-        cv2.imshow("mask", mask)
-        cv2.imshow("result", object_image)
+        object_image = np.zeros(
+            (object_black_image.shape[0], object_black_image.shape[1], 4))
+
+        for i in range(object_black_image.shape[0]):
+            for j in range(object_black_image.shape[1]):
+                if (object_black_image[i][j] == [0, 0, 0]).all():
+                    continue
+                object_image[i][j][:3] = object_black_image[i][j]
+                object_image[i][j][3] = 255
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         return object_image
 
-    def cutImageFileObject(self, image_file_path, color_mode):
+    def getObjectImageFromImageFile(self, image_file_path, color_mode="green"):
         assert os.path.exists(image_file_path)
-        assert color_mode in H_RANGE_DICT
+        assert color_mode in H_RANGE_DICT.keys()
 
         image = cv2.imread(image_file_path)
-        return self.cutImageObject(image, color_mode)
+        return self.getObjectImage(image, color_mode)
+
+    def cutImageFileObject(self,
+                           image_file_path,
+                           save_object_image_file_path,
+                           color_mode="green"):
+        assert os.path.exists(image_file_path)
+        assert color_mode in H_RANGE_DICT.keys()
+
+        object_image = self.getObjectImageFromImageFile(
+            image_file_path, color_mode)
+
+        createFileFolder(save_object_image_file_path)
+
+        cv2.imwrite(save_object_image_file_path, object_image)
+        return True
