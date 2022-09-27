ad3fdfd2bc98ee604383cc363081ae4c21125084
chLi <565353780@qq.com>
2022-09-27 15:09:25 +0800

    add some basic color range

---
+++ image_object_cut/Config/color.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+BLACK_H_RANGE = [[0, 0, 0], [180, 255, 46]]
+GRAY_H_RANGE = [[0, 0, 46], [180, 43, 220]]
+WHITE_H_RANGE = [[0, 0, 221], [180, 30, 255]]
+RED1_H_RANGE = [[0, 43, 46], [10, 255, 255]]
+RED2_H_RANGE = [[156, 43, 46], [180, 255, 255]]
+ORANGE_H_RANGE = [[11, 43, 46], [25, 255, 255]]
+YELLOW_H_RANGE = [[26, 43, 46], [34, 255, 255]]
+GREEN_H_RANGE = [[35, 43, 46], [77, 255, 255]]
+CYAN_H_RANGE = [[78, 43, 46], [99, 255, 255]]
+BLUE_H_RANGE = [[100, 43, 46], [124, 255, 255]]
+PURPLE_H_RANGE = [[125, 43, 46], [155, 255, 255]]
+
+H_RANGE_DICT = {
+    "black": BLACK_H_RANGE,
+    "gray": GRAY_H_RANGE,
+    "white": WHITE_H_RANGE,
+    "red1": RED1_H_RANGE,
+    "red2": RED2_H_RANGE,
+    "orange": ORANGE_H_RANGE,
+    "yellow": YELLOW_H_RANGE,
+    "green": GREEN_H_RANGE,
+    "cyan": CYAN_H_RANGE,
+    "blue": BLUE_H_RANGE,
+    "purple": PURPLE_H_RANGE
+}
