091335afbf38c828a64edd63be2db2236c92e075
chLi <565353780@qq.com>
2022-09-27 16:05:39 +0800

    finish cut image folder algo

--- image_object_cut/Module/image_object_cutter.py
+++ image_object_cut/Module/image_object_cutter.py
@@ -5,9 +5,10 @@ import os
 
 import cv2
 import numpy as np
+from tqdm import tqdm
 
 from image_object_cut.Config.color import H_RANGE_DICT
-from image_object_cut.Method.path import createFileFolder
+from image_object_cut.Method.path import createFileFolder, renameFile
 
 
 class ImageObjectCutter(object):
@@ -40,8 +41,6 @@ class ImageObjectCutter(object):
                 object_image[i][j][:3] = object_black_image[i][j]
                 object_image[i][j][3] = 255
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
         return object_image
 
     def getObjectImageFromImageFile(self, image_file_path, color_mode="green"):
@@ -49,6 +48,7 @@ class ImageObjectCutter(object):
         assert color_mode in H_RANGE_DICT.keys()
 
         image = cv2.imread(image_file_path)
+
         return self.getObjectImage(image, color_mode)
 
     def cutImageFileObject(self,
@@ -64,4 +64,45 @@ class ImageObjectCutter(object):
         createFileFolder(save_object_image_file_path)
 
         cv2.imwrite(save_object_image_file_path, object_image)
+        return True
+
+    def cutImageFolderObject(self,
+                             image_folder_path,
+                             save_object_image_folder_path,
+                             color_mode="green",
+                             print_progress=False):
+        assert os.path.exists(image_folder_path)
+        file_name_list = os.listdir(image_folder_path)
+
+        image_file_name_list = []
+        for file_name in file_name_list:
+            if file_name[-4:] not in [".jpg", ".png"]:
+                continue
+            image_file_name_list.append(file_name)
+
+        if len(image_file_name_list) == 0:
+            return True
+
+        os.makedirs(save_object_image_folder_path, exist_ok=True)
+
+        for_data = image_file_name_list
+        if print_progress:
+            print("[INFO][ImageObjectCutter::cutImageFolderObject]")
+            print("\t start cut image file object...")
+            for_data = tqdm(for_data)
+        for image_file_name in for_data:
+            image_file_path = image_folder_path + image_file_name
+            save_object_image_file_path = save_object_image_folder_path + image_file_name.split(".")[0] + ".png"
+
+            if os.path.exists(save_object_image_file_path):
+                continue
+
+            tmp_save_object_image_file_path = save_object_image_file_path[:
+                                                                          -4] + "_tmp.png"
+            assert self.cutImageFileObject(image_file_path,
+                                           tmp_save_object_image_file_path,
+                                           color_mode)
+
+            renameFile(tmp_save_object_image_file_path,
+                       save_object_image_file_path)
         return True
