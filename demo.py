37f590e7d10758e051f77df45c4eb326bb34da0e
chLi <565353780@qq.com>
2022-09-27 16:05:47 +0800

    add new cut image folder demo

--- demo.py
+++ demo.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from image_object_cut.Demo.image_object_cutter import demo
+from image_object_cut.Demo.image_object_cutter import \
+    demo as demo_cut_image, \
+    demo_folder as demo_cut_image_folder
 
 if __name__ == "__main__":
-    demo()
+    #  demo_cut_image()
+    demo_cut_image_folder()
